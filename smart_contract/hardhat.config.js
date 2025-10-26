33511b55dd3b0810b56c668e9c2a38fbb0c214f5
theeliteclub2k <vanjregautham@gmail.com>
2025-10-26 14:26:42 +0530

    feat: hardhat setup

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
